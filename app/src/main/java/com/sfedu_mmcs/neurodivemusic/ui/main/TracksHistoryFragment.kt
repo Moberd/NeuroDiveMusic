993aec8fe147bd6c873a6555df8ec6abfc7ae8e3
a.taranenko <i_okatv_i@mail.ru>
2024-06-02 16:18:17 +0300

    History\Favorites update

--- app/src/main/java/com/sfedu_mmcs/neurodivemusic/ui/main/TracksHistoryFragment.kt
+++ app/src/main/java/com/sfedu_mmcs/neurodivemusic/ui/main/TracksHistoryFragment.kt
@@ -2,23 +2,21 @@ package com.sfedu_mmcs.neurodivemusic.ui.main
 
 import android.app.AlertDialog
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.Observer
-import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.sfedu_mmcs.neurodivemusic.R
 import com.sfedu_mmcs.neurodivemusic.databinding.FragmentTracksHistrotyBinding
 import com.sfedu_mmcs.neurodivemusic.viewmodels.history.HistoryViewModel
 import com.sfedu_mmcs.neurodivemusic.viewmodels.history.model.HistoryTrackData
-import com.sfedu_mmcs.neurodivemusic.viewmodels.music.model.TrackData
+
 
 class TracksHistoryFragment : Fragment() {
     private lateinit var binding: FragmentTracksHistrotyBinding
@@ -50,6 +48,14 @@ class TracksHistoryFragment : Fragment() {
             adapter = historyListAdapter
             layoutManager = LinearLayoutManager(this@TracksHistoryFragment.context)
         }
+        val itemDecorator = DividerItemDecoration(context, DividerItemDecoration.VERTICAL)
+        itemDecorator.setDrawable(context?.let {
+            ContextCompat.getDrawable(
+                it,
+                R.drawable.tracks_history_list_divider
+            )
+        }!!)
+        binding.tracksHistoryList.addItemDecoration(itemDecorator)
     }
 
     fun showDialog(trackData: HistoryTrackData) {
